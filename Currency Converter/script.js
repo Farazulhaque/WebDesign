207ca581e695eeba2bd4bba449e991499dca2f84
Faraz <farazattari786@gmail.com>
2021-07-11 06:55:48 +0530

    added some more currencies

--- Currency Converter/script.js
+++ Currency Converter/script.js
@@ -14,13 +14,28 @@ function myfunction() {
   var to = document.getElementById("tocurrency");
   var tocurrency = to.options[to.selectedIndex].text;
   var output;
+  var ratio;
 
   if (fromcurrency == "Euro" && tocurrency == "Turkish Lira") {
     output = n * 9.39;
-  } else if (fromcurrency == "Turkish Lira" && tocurrency == "Euro") {
+  }
+  else if (fromcurrency == "Turkish Lira" && tocurrency == "Euro") {
     output = n * 0.11;
   }
+  else if (fromcurrency == "Euro" && tocurrency == "US Dollar") {
+    output = n * 1.19;
+  }
+  else if (fromcurrency == "US Dollar" && tocurrency == "Euro") {
+    output = n * 0.84;
+  }
+  else if (fromcurrency == "Euro" && tocurrency == "Canadian Dollar") {
+    output = n * 1.48;
+  }
+  else if (fromcurrency == "Canadian Dollar" && tocurrency == "Euro") {
+    output = n * 0.68;
+  }
 
+  ratio = output / n;
   document.getElementById("output").innerHTML =
-    n + " " + fromcurrency + " = " + output + " " + tocurrency;
+    n + " " + fromcurrency + " = " + output.toFixed(2) + " " + tocurrency + " (" + ratio + ":1)";
 }
